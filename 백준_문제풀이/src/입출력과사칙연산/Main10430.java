747c718d07c4e542b45207ebd85329bbeae99367
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-10 18:53:53 +0900

    백준 10430번 풀이

---
+++ 백준_문제풀이/src/입출력과사칙연산/Main10430.java
@@ -0,0 +1,23 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class Main10430 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+		
+		scan.close();
+	}
+
+}
