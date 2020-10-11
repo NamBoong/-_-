e45021451ae2409dc7973133c3be9623cb615b11
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-11 20:49:03 +0900

    백준 11021번 풀이

---
+++ 백준_문제풀이/src/for문/Main11021.java
@@ -0,0 +1,27 @@
+package for¹®;
+
+import java.util.Scanner;
+
+public class Main11021 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		int[][] arr = new int[a][2];
+		
+		for(int i=0; i<a; i++) {
+			for(int j=0; j<2; j++) {
+				arr[i][j] = scan.nextInt();
+			}
+		}
+		
+		for(int i=0; i<a; i++) {
+			System.out.println("Case #" + (i+1) + ": " + (arr[i][0]+arr[i][1]));
+		}
+		scan.close();
+	}
+
+}
