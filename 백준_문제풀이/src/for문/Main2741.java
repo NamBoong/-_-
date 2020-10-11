9220288dbe3a106b01e262451c95b577536d6220
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-11 16:09:32 +0900

    백준 2741번 풀이

---
+++ 백준_문제풀이/src/for문/Main2741.java
@@ -0,0 +1,20 @@
+package for¹®;
+
+import java.util.Scanner;
+
+public class Main2741 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(i);
+		}
+		
+		scan.close();
+	}
+
+}
