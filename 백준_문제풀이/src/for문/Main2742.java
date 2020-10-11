009fadc028b39d7e428231d40ff178323c5951d6
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-11 16:10:58 +0900

    백준 2742번 풀이

---
+++ 백준_문제풀이/src/for문/Main2742.java
@@ -0,0 +1,20 @@
+package for¹®;
+
+import java.util.Scanner;
+
+public class Main2742 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=n; i>=1; i--) {
+			System.out.println(i);
+		}
+		
+		scan.close();
+	}
+
+}
