2e5cc2fa55fd47d7b356b135128788f74e60f910
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-12 21:50:04 +0900

    백준 11654번 풀이

---
+++ 백준_문제풀이/src/문자열/Main11654.java
@@ -0,0 +1,19 @@
+package ¹®ÀÚ¿­;
+
+import java.util.Scanner;
+
+public class Main11654 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.next();
+		int b = a.charAt(0);
+		
+		System.out.println(b);
+		
+		scan.close();
+	}
+
+}
