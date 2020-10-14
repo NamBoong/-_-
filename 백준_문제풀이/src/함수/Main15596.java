ab330bc56a50948931965756aff5665deae9cdbf
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-14 17:46:36 +0900

    백준 15596번 풀이

---
+++ 백준_문제풀이/src/함수/Main15596.java
@@ -0,0 +1,26 @@
+package 함수;
+
+public class Main15596 {
+
+	//public static void main(String[] args) {
+	
+	//제출 할 때 이 부분까지는 지웠습니다.
+
+	
+	//이부분부터 코드 제출
+		//public class Test {
+		    long sum(int[] a) {
+		        long ans = 0;
+		        
+		        for(int i=0; i<a.length; i++){
+		            ans = ans+a[i];
+		        }
+		        return ans;
+		    }
+		//}
+
+		    
+	// 아래로는 코드를 제출하지 않았습니다.
+	}
+
+//}
