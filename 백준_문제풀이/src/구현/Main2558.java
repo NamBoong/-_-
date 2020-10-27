ada324b09aa1c35df4f386771a1cc7b36a0955e6
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-28 00:11:05 +0900

    백준 2558번 풀이

---
+++ 백준_문제풀이/src/구현/Main2558.java
@@ -0,0 +1,24 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main2558 {
+	
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		answer(a, b);
+		
+		scan.close();
+	}
+	
+	public static void answer(int a, int b) {
+		
+		System.out.println(a+b);
+	}
+
+}
