031bfcbd1fdf5ffe311c738cda0aaba5b180b434
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-28 15:53:31 +0900

    백준 11719번 풀이

---
+++ 백준_문제풀이/src/구현/Main11719.java
@@ -0,0 +1,19 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main11719 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNextLine()) {
+			String a = scan.nextLine();
+			System.out.println(a);
+		}
+		
+		scan.close();
+	}
+
+}
