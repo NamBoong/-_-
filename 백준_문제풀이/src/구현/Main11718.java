623bd94410b7861166a905f57ac30b82330c73d3
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-28 15:49:56 +0900

    백준 11718번 풀이

---
+++ 백준_문제풀이/src/구현/Main11718.java
@@ -0,0 +1,19 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main11718 {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+		while(scan.hasNextLine()) {
+			String a = scan.nextLine();
+			System.out.println(a);
+		}
+		
+		scan.close();
+	}
+
+}
