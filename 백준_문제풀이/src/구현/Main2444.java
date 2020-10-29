6a5b4c8c8ed7cc8fba33a07614f33c4246cb1b81
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-30 00:37:06 +0900

    백준 2444번 풀이

---
+++ 백준_문제풀이/src/구현/Main2444.java
@@ -0,0 +1,55 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main2444 {
+
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+		int n = scan.nextInt();
+		
+		answer(n);
+		
+		scan.close();
+	}
+	
+	public static void answer(int n) {
+		
+		int count = 0;
+		
+		while(count<n) {
+			
+			for(int i=n-count; i>1; i--) {
+				System.out.printf(" ");
+			}
+			
+			for(int i=0; i<count*2+1; i++) {
+				System.out.printf("*");
+			}
+			
+			System.out.println();
+			count++;
+		}
+		
+		int count2 = 1;
+		
+		while(count2<=n) {
+			
+			for(int i=0; i<count2; i++) {
+				System.out.printf(" ");
+			}
+			
+			for(int i=0; i<count*2-3; i++) {
+				System.out.printf("*");
+			}
+			
+			System.out.println();
+			count2++;
+			count--;
+		}
+
+	}
+
+}
