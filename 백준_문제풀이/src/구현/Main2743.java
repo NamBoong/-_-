8b6f01a113987c0b593c5a144da42e10855493ed
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-30 00:41:19 +0900

    백준 2743번 풀이

---
+++ 백준_문제풀이/src/구현/Main2743.java
@@ -0,0 +1,22 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main2743 {
+
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+		String n = scan.nextLine();
+		
+		answer(n);
+		scan.close();
+	}
+	
+	public static void answer(String n) {
+		
+		System.out.println(n.length());
+	}
+
+}
