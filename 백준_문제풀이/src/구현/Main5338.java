eed0d503dda9fc6972d509a9157dccb381816e7e
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-30 18:56:52 +0900

    백준 5338번 풀이

---
+++ 백준_문제풀이/src/구현/Main5338.java
@@ -0,0 +1,14 @@
+package ±¸Çö;
+
+public class Main5338 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
