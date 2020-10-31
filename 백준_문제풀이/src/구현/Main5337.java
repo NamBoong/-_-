8904cfdc781f738b06892754a38ddeb81c300507
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-31 14:50:42 +0900

    백준 5337번 풀이

---
+++ 백준_문제풀이/src/구현/Main5337.java
@@ -0,0 +1,12 @@
+package ±¸Çö;
+
+public class Main5337 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
