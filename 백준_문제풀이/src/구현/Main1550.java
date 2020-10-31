ab7b4777b82ae48400e2c27c703e99d66d4a901b
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-10-31 17:32:39 +0900

    백준 1550번 풀이

---
+++ 백준_문제풀이/src/구현/Main1550.java
@@ -0,0 +1,17 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main1550 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.next();
+		
+		System.out.println(Integer.parseInt(a, 16));
+		scan.close();
+	}
+
+}
