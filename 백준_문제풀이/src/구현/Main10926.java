0e2e42f9384af4b6e88b14d0dc567ede1bc2d174
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-02 15:52:01 +0900

    백준 10926번 풀이

---
+++ 백준_문제풀이/src/구현/Main10926.java
@@ -0,0 +1,22 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main10926 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.next();
+		
+		answer(a);
+		scan.close();
+	}
+	
+	public static void answer(String a) {
+		
+		System.out.println(a + "??!");
+	}
+
+}
