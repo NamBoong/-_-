1ec276773b739f66da2ca8ba1e8cbae740c97f16
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-02 13:17:01 +0900

    백준 5339번 풀이

---
+++ 백준_문제풀이/src/구현/Main5339.java
@@ -0,0 +1,19 @@
+package ±¸Çö;
+
+public class Main5339 {
+
+	public static void main(String[] args) {
+
+		System.out.println("     /~\\\n" + 
+				"    ( oo|\n" + 
+				"    _\\=/_\n" + 
+				"   /  _  \\\n" + 
+				"  //|/.\\|\\\\\n" + 
+				" ||  \\ /  ||\n" + 
+				"============\n" + 
+				"|          |\n" + 
+				"|          |\n" + 
+				"|          |");
+	}
+
+}
