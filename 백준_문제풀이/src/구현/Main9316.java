ed12f764508182ee53cdd198011e43ed303aaf85
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-05 16:59:57 +0900

    백준 9316번 풀이

---
+++ 백준_문제풀이/src/구현/Main9316.java
@@ -0,0 +1,26 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main9316 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		answer(n);
+		
+		scan.close();
+	}
+	
+	public static void answer(int n) {
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println("Hello World, Judge " + i + "!");
+		}
+		
+	}
+
+}
