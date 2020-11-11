21ad39092d7f43fef250c8f293ba60dae4ae3c55
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-11 15:08:29 +0900

    백준 15353번 풀이

---
+++ 백준_문제풀이/src/구현/Main15353.java
@@ -0,0 +1,19 @@
+package ±¸Çö;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main15353 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		BigInteger a = scan.nextBigInteger();
+		BigInteger b = scan.nextBigInteger();
+		
+		System.out.println(a.add(b));
+		scan.close();
+	}
+
+}
