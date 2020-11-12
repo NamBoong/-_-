9773e15d7d8f5b0c79961f10fee74300f4a36dc4
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-12 18:47:04 +0900

    백준 1037번 풀이

---
+++ 백준_문제풀이/src/수학/Main1037.java
@@ -0,0 +1,33 @@
+package ¼öÇÐ;
+
+import java.util.Scanner;
+
+public class Main1037 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=i+1; j<n; j++) {
+				if(arr[i]>arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		System.out.println(arr[0]*arr[n-1]);
+		
+		scan.close();
+	}
+
+}
