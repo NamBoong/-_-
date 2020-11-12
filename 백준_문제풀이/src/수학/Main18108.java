19de6c1e0c5fec5b3dad6c9a274aaac2c507aebf
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-12 18:28:02 +0900

    백준 18108번 풀이

---
+++ 백준_문제풀이/src/수학/Main18108.java
@@ -0,0 +1,18 @@
+package ¼öÇÐ;
+
+import java.util.Scanner;
+
+public class Main18108 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		System.out.println(n-543);
+		
+		scan.close();
+	}
+
+}
