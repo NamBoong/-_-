eecfe0d974966739fca34ea2fb182a3a8bb3f9d4
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-15 23:51:51 +0900

    백준 14581번 풀이

---
+++ 백준_문제풀이/src/구현/Main14581.java
@@ -0,0 +1,24 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main14581 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.next();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + answer(a) + "::fan:");
+		System.out.println(":fan::fan::fan:");
+
+		scan.close();
+	}
+	
+	public static String answer(String a) {
+		return a;
+	}
+
+}
