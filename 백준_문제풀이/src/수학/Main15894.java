2a79c887c06d514c2f97aff559b1bc563be679f5
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-16 12:26:44 +0900

    백준 15894번 풀이

---
+++ 백준_문제풀이/src/수학/Main15894.java
@@ -0,0 +1,18 @@
+package ¼öÇÐ;
+
+import java.util.Scanner;
+
+public class Main15894 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		long a = scan.nextLong();
+		
+		System.out.println(a*4);
+		
+		scan.close();
+	}
+
+}
