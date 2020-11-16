cada272bca673f5b8984d32aeefc32234495122a
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-16 12:28:51 +0900

    백준 14918번 풀이

---
+++ 백준_문제풀이/src/수학/Main14918.java
@@ -0,0 +1,19 @@
+package ¼öÇÐ;
+
+import java.util.Scanner;
+
+public class Main14918 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+		
+		scan.close();
+	}
+
+}
