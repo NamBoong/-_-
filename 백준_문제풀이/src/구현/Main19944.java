f9e525cb7c24315678a1e447d54a38d15aea9b90
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-16 23:20:08 +0900

    백준 19944번 풀이

---
+++ 백준_문제풀이/src/구현/Main19944.java
@@ -0,0 +1,28 @@
+package ±¸Çö;
+
+import java.util.Scanner;
+
+public class Main19944 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		
+		
+		if(m<=2) {
+			System.out.println("NEWBIE!");
+		}
+		else if(m<=n && m>2) {
+			System.out.println("OLDBIE!");
+		}
+		else {
+			System.out.println("TLE!");
+		}
+		
+		scan.close();
+	}
+
+}
