321498741235322cc4c7054b3927bc92ad609fda
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-20 18:16:54 +0900

    백준 16483번 풀이

---
+++ 백준_문제풀이/src/수학/Main16483.java
@@ -0,0 +1,18 @@
+package ¼öÇÐ;
+
+import java.util.Scanner;
+
+public class Main16483 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int t = scan.nextInt();
+		
+		System.out.println(Math.round(Math.pow(t, 2)/4));
+		
+		scan.close();
+	}
+
+}
