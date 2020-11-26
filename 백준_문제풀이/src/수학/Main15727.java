6dbdd8509799392b7dc31a5b5e04d2295e99044a
NamBoong <67003390+NamBoong@users.noreply.github.com>
2020-11-26 12:03:44 +0900

    백준 15727번 풀이

---
+++ 백준_문제풀이/src/수학/Main15727.java
@@ -0,0 +1,17 @@
+package ¼öÇÐ;
+
+import java.util.Scanner;
+
+public class Main15727 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		long n = scan.nextLong();
+		
+		System.out.println((int)Math.ceil(n/5.0));
+		scan.close();
+	}
+
+}
